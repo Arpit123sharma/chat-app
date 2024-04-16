83864dc0bacb966bf93261ef374b441ab39ff5e5
Arpit123sharma <138675911+Arpit123sharma@users.noreply.github.com>
2024-04-16 21:28:03 +0530

    create user schema

--- Server/src/Models/user.model.js
+++ Server/src/Models/user.model.js
@@ -1,2 +1,30 @@
 import mongoose,{Schema} from "mongoose"
 
+const userSchema = Schema({
+    userName:{
+        type:String,
+        required:true,
+        lowercase:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        lowercase:true,
+        unique:true  
+    },
+    Number:{
+        type:Number,
+        required:true,
+        unique:true
+    },
+    dp:{
+        type:String,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+
+export default User = mongoose.model("User",userSchema);
